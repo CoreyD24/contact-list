d95d70417bcf1c294355e4f7276abd01fe8475a3
coreyfsa <coreyfsa@DESKTOP-TCPPHAT.localdomain>
2023-12-12 15:25:25 -0700

    Initial commit. Have the dummy contacts showing up on webpage. End of tab 2

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ContactList from "./components/ContactList"
+import ContactRow from './components/ContactRow'
+
+
+const App = () => {
+
+  return (
+    <>
+      <ContactList/>
+    </>
+  )
+}
+
+export default App
